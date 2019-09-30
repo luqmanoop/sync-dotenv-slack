29d5dfb50d50931cd368b83bbb408f9b5bdfaf6d
Luqman Olushi <olushilukmon03@gmail.com>
2019-09-30 18:44:27 +0100

    post sample msg to general channel

--- lib.ts
+++ lib.ts
@@ -1,1 +1,17 @@
-console.log('hello, world');
+import SlackBot from 'slackbots';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const bot = new SlackBot({
+  token: process.env.SLACK_TOKEN,
+  name: '.envBot'
+})
+
+const params = {
+  icon_emoji: ':dog:'
+}
+
+bot.on('start', () => {
+  bot.postMessage('general', 'Hello, world', params)
+})
