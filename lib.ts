cccf470203fb623d536307feb041b8fec29676af
Luqman Olushi <olushilukmon03@gmail.com>
2019-10-01 15:50:59 +0100

    get last message posted to channel

--- lib.ts
+++ lib.ts
@@ -1,5 +1,6 @@
 import { WebClient } from '@slack/web-api';
 import dotenv from 'dotenv';
+import parseDotenv from 'parse-dotenv';
 import fs from 'fs';
 
 dotenv.config();
@@ -11,10 +12,37 @@ const params = {
   icon_emoji: ':dog:',
   username: '.envBot'
 };
+interface IChannel {
+  name: string;
+  id: string;
+}
 
-const alertChannel = async (channel: string, text: string) => {
+interface IMessage {
+  text: string;
+  username: string;
+  bot_id: string;
+}
+
+const getChannels = async (): Promise<IChannel[]> => {
+  const { channels } = await web.conversations.list({ exclude_archived: true, types: 'public_channel,private_channel' });
+  return channels as IChannel[];
+}
+
+const getChannel = async (channelName: string): Promise<IChannel> => {
+  const channels = await getChannels();
+  return channels.filter(channel => channel.name === channelName)[0];
+}
+
+const getLatestMessage = async (channel: IChannel): Promise<IMessage> => {
+  const { messages } = await web.channels.history({ channel: channel.id, count: 1 });
+  return messages[0];
+}
+
+const alertChannel = async (channelName: string, text: string) => {
   try {
-    await web.chat.postMessage({ channel, text, token, ...params });
+    const channel = await getChannel(channelName);
+    const oldEnvPost = await getLatestMessage(channel);
+    await web.chat.postMessage({ channel: channel.name, text, ...params });
     process.exit(0);
   } catch (error) {
     process.exit(1);
@@ -24,5 +52,3 @@ const alertChannel = async (channel: string, text: string) => {
 const getEnv = (path: string = '.env') => {
   return fs.readFileSync(path, { encoding: 'utf-8' });
 };
-
-alertChannel('bot', getEnv());
